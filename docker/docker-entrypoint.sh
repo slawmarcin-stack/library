97b587b69254befe86d021af862561beb1275116
Marcin Sławiński <marcin.slawinski@codema.pl>
2026-07-01 21:45:55 +0200

    fix entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "Waiting for database to be ready..."
 
-until php -r "\$fp = @fsockopen('database', 5432, \$errno, \$errstr, 2); if (\$fp) { fclose(\$fp); exit(0); } else { exit(1); }" > /dev/null 2>&1; do
+DB_HOST_NAME=${DB_HOST:-database}
+
+until php -r "\$fp = @fsockopen('$DB_HOST_NAME', 5432, \$errno, \$errstr, 2); if (\$fp) { fclose(\$fp); exit(0); } else { exit(1); }" > /dev/null 2>&1; do
   sleep 1
 done
 
